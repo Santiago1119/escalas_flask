0bf2ce1d7177e9bacbcfab01b9c7bd0ec3fe0411
Santiago Gonzalez <parrasanti.gonzalez@gmail.com>
2023-02-19 19:41:21 -0500

    La busqueda de las respuestas de usuario se mejoró y muestra exactamente que respondio el usuario

--- api_phq9/app/phq_9.py
+++ api_phq9/app/phq_9.py
@@ -1,16 +1,55 @@
 from flask import Blueprint, jsonify, request
 from app import app, db
 from .models import AnswersPHQ9, ResultPHQ9
+import re
 
 phq9_bp = Blueprint('phq9', __name__, url_prefix='/phq9')
 
 @phq9_bp.route('/answers/<int:user_id>', methods=['GET'])
-def get_answers(user_id):
+def get_answers(user_id:int)->jsonify:
+    """Consulta las respuestas que ingresó el usuario en el formulario PHQ-9
+
+    Args:
+        user_id (int): id del usuario
+
+    Returns:
+        jsonify: archivo json con el valor de cada respuésta del usuario
+    """
+    def question_value(answers:object)->str:
+        answers_dict = [answer.to_dict() for answer in answers]
+        user_response = {}
+        
+        for answer_dict in answers_dict:
+            for value in answer_dict:
+                if re.match(r"^answer_\d+$", value) and answer_dict[value] == 0:
+                    user_response[value] = 'Ningún día'
+                elif re.match(r"^answer_\d+$", value) and answer_dict[value] == 1:
+                    user_response[value] = 'Varios días'
+                elif re.match(r"^answer_\d+$", value) and answer_dict[value] == 2:
+                    user_response[value] = 'Más de la mitad de los días'
+                elif re.match(r"^answer_\d+$", value) and answer_dict[value] == 3:
+                    user_response[value] = 'Casi todos los días'
+
+            
+        return user_response
+    
+    
     answers = AnswersPHQ9.query.filter_by(user_id= user_id)
-    return jsonify({'answers': [answer.to_dict() for answer in answers]})
+    
+    user_responses = question_value(answers)
+    return jsonify({'answers': user_responses,
+                    'db_values': [answer.to_dict() for answer in answers]})
+
+
 
 @phq9_bp.route('/register', methods=['POST'])
-def register_phq9():
+def register_phq9()->jsonify:
+    """registra en la base de datos los valores de cada pregunta y calcula el total que sacó en la prueba
+
+    Returns:
+        jsonify: archivo json con el id de usuario, las respuestas del formulario, y el resultado de la escala
+    """
+    
     user_id = request.json.get('user_id')
     answer_1 = request.json.get('answer_1')
     answer_2 = request.json.get('answer_2')
